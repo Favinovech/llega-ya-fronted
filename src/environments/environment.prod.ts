ba83c4494481f9e7d6a5098a48dc79347a0c370c
Angel Asmat <asmatdzevallos@gmail.com>
2026-06-21 14:21:59 -0500

    fix: ajustar budgets y corregir environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.llegaya.pe/api/auth',
+  apiUrl: 'https://llegaya-backend.onrender.com/api/auth',
   appName: 'LlegaYa',
   appSlogan: 'Conectando barrios con un solo clic',
 };
