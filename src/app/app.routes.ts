9f3e4f44c2589aa4f094084569e03e4babde2b42
Favinovech <favinovech@gmail.com>
2026-07-07 13:45:27 -0500

    app-routes-prediccion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -41,6 +41,8 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/incidencias/incidencias').then(m => m.Incidencias) },
   { path: 'admin/reportes', canActivate: [authGuard, adminGuard],
     loadComponent: () => import('./pages/reporte-diario/reporte-diario').then(m => m.ReporteDiarioPage) },
+  { path: 'admin/prediccion', canActivate: [authGuard, adminGuard],
+    loadComponent: () => import('./pages/prediccion-demanda/prediccion-demanda').then(m => m.PrediccionDemandaPage) },
 
   // Solo repartidor
   { path: 'repartidor', canActivate: [repartidorGuard],
